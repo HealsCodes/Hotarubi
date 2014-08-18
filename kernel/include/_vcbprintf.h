5a3c490f769e2ac6020a25e211ae3d69ef1d2c75
René Köcher <shirk@bitspin.org>
2014-08-18 10:25:57 +0200

    There's a lot more space now!

--- kernel/include/_vcbprintf.h
+++ kernel/include/_vcbprintf.h
@@ -1,21 +1,21 @@
 /*******************************************************************************
 
-	Copyright (C) 2014  René 'Shirk' Köcher
+    Copyright (C) 2014  René 'Shirk' Köcher
  
-	This file is part of Hotarubi.
+    This file is part of Hotarubi.
 
-	Hotarubi is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+    Hotarubi is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
  
-	Hotarubi is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+    Hotarubi is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
  
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 *******************************************************************************/
 
@@ -36,7 +36,7 @@ int _vcbprintf( void *p, size_t ( *cb )( void *p, const char *str, size_t n ),
                 const char *fmt, va_list ap );
 
 int _cbprintf( void *p, size_t ( *cb )( void *p, const char *str, size_t n ),
-               const char *fmt, ... );
+               const char *fmt, ... ) __attribute__( ( format( printf, 3, 4 ) ) );
 
 END_C_DECL
 
